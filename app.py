ae9618b0c9d6ef5e2f1b13acb3314319ce434275
auxfuse <aux.fuse@yahoo.com>
2019-11-01 23:33:00 +0000

    html files and templates folder to hold same created

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
-#
-# if __name__ == "__main__":
-#     app.run(host=os.environ.get("IP"),
-#             port=int(os.environ.get("PORT")),
-#             debug=True)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=(os.environ.get("PORT")),
+            debug=True)
